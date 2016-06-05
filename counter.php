af824c87f16e91bdee142809d72a981824189de2
EMTipp <testit.2000@mail.ru>
2016-06-05 20:29:19 +0200

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,9 +1,19 @@
 <?php
-$count_my_page = ("hitcounter.txt");
-$hits = file($count_my_page);
-$hits[0] ++;
-$fp = fopen($count_my_page , "w");
-fputs($fp , "$hits[0]");
-fclose($fp);
-echo $hits[0];
+
+/* counter */
+
+//opens countlog.txt to read the number of hits
+$datei = fopen("/countlog.txt","r");
+$count = fgets($datei,1000);
+fclose($datei);
+$count=$count + 1 ;
+echo "$count" ;
+echo " hits" ;
+echo "\n" ;
+
+// opens countlog.txt to change new hit number
+$datei = fopen("/countlog.txt","w");
+fwrite($datei, $count);
+fclose($datei);
+
 ?>
